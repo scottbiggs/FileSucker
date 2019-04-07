bc56d43e3e54d5cb18aa636085e0a5e074b3649c
sbiggs <scott.biggs@consensus-builder.com>
2019-04-06 22:03:24 -0500

    now downloads successfully ONE file

--- FileSucker.java
+++ FileSucker.java
@@ -5,6 +5,9 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;//import org.apache.commons.io.FileUtils;
 
+import java.io.FileOutputStream;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
 
 /**
  * Quick program to scrounge a bunch of sequentially numbered files off the
@@ -21,9 +24,12 @@ public class FileSucker {
 
 	private static final String USAGE =
 		"USAGE:\n"
-		+ "\tfilesucker <url-prefix> <start_num> <suffix>\n"
+		+ "\tfilesucker <url> || (<url-prefix> <start_num> <suffix>)\n"
 		+ "\n"
-		+ "Downloads a number of sequential files from a given url.\n"
+		+ "If just the <url> is provided, this will simply download the url\n"
+		+ "to the current directory.\n"
+		+ "\n"
+		+ "Otherwise this downloads a number of sequential files from a given url's previx.\n"
 		+ "These files will be put in the current directory.\n"
 		+ "All sequential files will be downloaded until an error is received\n"
 		+ "(file not found) or the process is killed.\n"
@@ -47,6 +53,12 @@ public class FileSucker {
 	//-----------------------
 	//	data
 	//-----------------------
+
+	/** indicates the mode: just one file or a bunch of 'em */
+	private static boolean m_justOne = false;
+
+	/** The URL for the single file download.  Contains all that's needed. */
+	private static String m_justUrl;
 
 	/** the url param as supplied by user */
 	private static String m_urlPrefix;
@@ -74,6 +86,17 @@ public class FileSucker {
 			return;
 		}
 
+		if (m_justOne) {
+			String filename = extractFileFromUrl(m_justUrl);
+			if (downloadFile(m_justUrl, filename)) {
+				System.out.println("Successfully downloaded " + filename);
+			}
+			else {
+				System.out.println("Unable to download " + filename);
+			}
+			return;	// done!
+		}
+
 		System.out.println("m_urlPrefix = " + m_urlPrefix
 						   + ", m_startNum = " + m_startNum
 						   + ", m_extension = " + m_extension);
@@ -82,7 +105,7 @@ public class FileSucker {
 
 		for (count = 0; count < MAX_FILES; count++) {
 			String newUrl = getFullUrl();
-			String newFilename = getFilename();
+			String newFilename = getFilenameFromMemberData();
 
 			if (downloadFile(newUrl, newFilename) == false) {
 				break;
@@ -91,8 +114,6 @@ public class FileSucker {
 		}
 		System.out.println("Saved " + count + " files.");
 
-		// testing
-		System.out.println("getFullUrl = " + getFullUrl() + ", filename = " + getFilename());
 	}
 
 
@@ -100,7 +121,21 @@ public class FileSucker {
 	 * Returns TRUE iff successfully downloaded the specified file to the given filename.
 	 */
 	private static boolean downloadFile (String url_str, String filename) {
-		return false;
+		try {
+			URL website = new URL(url_str);
+			ReadableByteChannel rbc = Channels.newChannel(website.openStream());
+
+			FileOutputStream fos = new FileOutputStream(filename);
+			fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+
+			fos.close();
+			rbc.close();
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
 	}
 
 
@@ -114,22 +149,65 @@ public class FileSucker {
 	/**
 	 * Figures out the filename from the current state of the member data
 	 */
-	private static String getFilename() {
+	private static String getFilenameFromMemberData() {
 
 		int i = 0;
 		int urlPrefixLength = m_urlPrefix.length();
 		char ch = m_urlPrefix.charAt(urlPrefixLength - 1);	// get last char (position 0 in this case)
 
 		// work backwards until a non-allowable char is found
-		while ((i < m_urlPrefix.length()) && (isLegalFileChar(ch))) {
+//		while ((i < m_urlPrefix.length()) && (isLegalFileChar(ch))) {
+		while ((i < m_urlPrefix.length()) && (Character.isLetterOrDigit(ch))) {
 			i++;
 			ch = m_urlPrefix.charAt(urlPrefixLength - (1 + i));
 		}
 
-//		System.out.println ("getFilename: i = " + i);
-
-		String namePrefix = m_urlPrefix.substring(urlPrefixLength - i);	// todo: might be off by 1
+		String namePrefix = m_urlPrefix.substring(urlPrefixLength - i);
 		return namePrefix + m_num + "." + m_extension;
+	}
+
+	/**
+	 * Given a String that represents the URL of some website, this works backwards,
+	 * find a reasonable filename to save the URL as.
+	 *
+	 * Assumes that filenames are alphanumeric and can contain periods, dashes,
+	 * and underscores (no spaces--I'm feeling lazy).
+	 *
+	 * Very similar to getFilenameFromMemberData()!  Ha!
+	 *
+	 * Returns null if input is null or empty.
+	 */
+	private static String extractFileFromUrl (String url) {
+
+		System.out.println("entering: url = " + url);
+
+		if ((url == null) || (url.length() == 0)) {
+			return null;	// nothing to parse
+		}
+
+		int i = 0;
+		int url_length = url.length();
+
+//		System.out.println("url_length = " + url_length);
+		char ch = url.charAt(url_length - 1);
+
+		while ((i < url.length()) && (isLegalFileChar(ch))) {
+//		while ((i < url.length())
+//				&& ((Character.isLetterOrDigit(ch)
+//					|| (ch == '.')
+//					|| (ch == '+')
+//					|| (ch == '_')))) {
+//			System.out.println("  i = " + i + ", ch = " + ch);
+			i++;
+			ch = url.charAt(url_length - (1 + i));
+		}
+
+//		System.out.println("done with loop: i = " + i);
+
+		String filename = url.substring(url_length - i);
+
+//		System.out.println("exit: filename = " + filename);
+		return filename;
 	}
 
 	/**
@@ -138,11 +216,22 @@ public class FileSucker {
 	 * and FALSE is returned.
 	 */
 	private static boolean parseParams(String[] args) {
+
+		// Start with the simpler case.
+		if (args.length == 1) {
+			m_justOne = true;
+			m_justUrl = args[0];
+			return true;
+		}
+
+		// Check for the more complex case.
 		if (args.length != 3) {
+			// No? then tell 'em how to use this program
 			System.out.print(USAGE);
 			return false;
 		}
 
+		// Fill in the blanks
 		m_urlPrefix = args[0];
 		m_startNum = args[1];
 		m_extension = args[2];
@@ -160,6 +249,13 @@ public class FileSucker {
 
 	private static boolean isLegalFileChar(char c) {
 		boolean val = Character.isLetterOrDigit(c);
+		switch (c) {
+			case '.':
+			case '_':
+			case '+':
+				val = true;
+				break;
+		}
 //		System.out.println ("isLegalFileChar ( " + c + " ) is " + val);
 		return val;
 	}
